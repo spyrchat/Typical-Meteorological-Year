c1157da6b930d22bb84423f950aac41101afa9ff
spyrchat <spiroshatzi2002@gmail.com>
2023-12-23 21:45:14 +0100

    test works fine

---
+++ test.py
@@ -0,0 +1,76 @@
+import urllib.request, urllib.parse, urllib.error
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+ΟΝΟΜΑΤΕΠΩΝΥΜΟ: Χρήστος Μπιγδέλης
+ΑΜ: 151930
+"""
+
+# <--- ΕΔΩ ΘΑ ΜΠΟΥΝ ΟΙ ΒΙΒΛΙΟΘΗΚΕΣ ΠΟΥ ΘΑ ΧΡΗΣΙΜΟΠΟΙΉΣΕΤΕ
+
+def get_tmy(lati, long):
+    time_UTC, WD10m = [], []
+
+    try:
+        fhand = urllib.request.urlopen(f'https://re.jrc.ec.europa.eu/api/tmy?lat={lati}&lon={long}')
+
+        for line in fhand:
+            line = line.decode().strip()
+            if line.startswith("20"):  # Check for data lines
+                words = line.split(',')
+                time_UTC.append(words[0])
+                WD10m.append(float(words[8]))  # Assuming 9th item is wind direction
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
+
+    # Process wind direction data
+    hourly_wind_data = {hour: [] for hour in range(24)}
+
+    for i, wind_direction in enumerate(WD10m):
+        try:
+            hour = int(time_UTC[i].split(':')[1][:2])  # Extract hour
+            hourly_wind_data[hour].append(wind_direction)
+        except Exception as e:
+            print(f"Error processing time_UTC[{i}]: {time_UTC[i]}, Error: {e}")
+
+    # Calculate mean wind direction per hour
+    mean_directions = {}
+    for hour, directions in hourly_wind_data.items():
+        # Convert degrees to vectors
+        vectors = [(math.cos(math.radians(d)), math.sin(math.radians(d))) for d in directions]
+        # Calculate mean vector
+        mean_vector = np.mean(vectors, axis=0)
+        # Convert vector back to degrees
+        radians = math.atan2(mean_vector[1], mean_vector[0])
+        mean_direction = math.degrees(radians)
+        mean_direction = mean_direction if mean_direction >= 0 else mean_direction + 360
+        mean_directions[hour] = mean_direction
+
+    return mean_directions
+
+
+
+def plot_mean_wd_per_hour(wd_list):
+    hours = list(mean_wind_directions.keys())
+    directions = list(mean_wind_directions.values())
+
+    plt.scatter(hours, directions)
+    plt.xlabel('Hour of the Day')
+    plt.ylabel('Mean Wind Direction (Degrees)')
+    plt.title('Mean Wind Direction Per Hour')
+    plt.grid(True)
+    plt.text(0, -20, "Χρήστος Μπιγδέλης, 151930")
+    # Saving the plot as a JPEG file
+    # plt.savefig(f'C:\\dewp2023{151930}_graph3.jpeg')
+
+    plt.show()
+
+# ΣΧΟΛΙΑΣΕΤΕ ΤΟΝ ΚΩΔΙΚΑ ΣΑΣ
+
+if __name__ == "__main__":
+    mean_wind_directions = get_tmy(41.141816091983856, 24.891168951039393)
+    print(mean_wind_directions)
+    plot_mean_wd_per_hour(mean_wind_directions)
